da89f3bf6179b8243b1a1f3c95685215d302fa70
nscarcella <devnull@localhost>
2013-06-15 10:54:15 -0300

    * Purged remaining references to collision groups

--- src/main/scala/org/uqbar/chocolate/utils/commoncomponents/ui/UIComponent.scala
+++ src/main/scala/org/uqbar/chocolate/utils/commoncomponents/ui/UIComponent.scala
@@ -8,21 +8,12 @@ import org.uqbar.chocolate.core.components.Visible
 import org.uqbar.chocolate.core.components.Collisionable
 
 object UIComponent {
-	final val UI_COMPONENT_CG = 7772
 	final val MAIN_ACTION = "UI Main Action"
 }
 
 trait UIComponent extends Visible with Collisionable {
-
-	val collisionGroup = UI_COMPONENT_CG
-
-	// ****************************************************************
-	// ** TRIGGERS
-	// ****************************************************************
-
 	@OnCustomEvent(MAIN_ACTION)
 	def reactToMainAction = performMainAction
 
 	def performMainAction
-
 }
--- src/main/scala/org/uqbar/chocolate/utils/commoncomponents/ui/UICursor.scala
+++ src/main/scala/org/uqbar/chocolate/utils/commoncomponents/ui/UICursor.scala
@@ -6,17 +6,10 @@ import org.uqbar.chocolate.core.reactions.annotations.io.enums.MouseButton
 import org.uqbar.chocolate.core.reactions.annotations.io.mouse.OnMousePressed
 import org.uqbar.chocolate.core.reactions.events.CustomEvent
 import org.uqbar.chocolate.utils.commoncomponents.Cursor
-import UICursor._
 import org.uqbar.chocolate.core.components.Collisionable
 import org.uqbar.chocolate.core.components.Visible
 
-object UICursor {
-	final val UI_CURSOR_CG = 7773
-}
-
 class UICursor(val appearance : Appearance, val boundingBox : BoundingBox) extends Cursor with Visible with Collisionable {
-
-	def collisionGroup = UI_CURSOR_CG
 
 	// ****************************************************************
 	// ** TRIGGERS
